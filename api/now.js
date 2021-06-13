b926a98c6f02805888f1abf4800689396f007fec
realwuliao <fjxyjfy@gmail.com>
2021-06-13 22:52:52 +0800

    Update now.js

--- api/now.js
+++ api/now.js
@@ -1,4 +1,4 @@
-TZ='asia/shanghai'
+process.env.TZ = 'Asia/Shanghai';
 const path = require('path');
 const moduleAlias = require('module-alias');
 moduleAlias.addAlias('@', path.join(__dirname, '../lib'));
